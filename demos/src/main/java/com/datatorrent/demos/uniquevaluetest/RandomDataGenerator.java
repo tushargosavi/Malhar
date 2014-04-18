3f221867b6e88bc856700c9a091da96e88fa2eb1
Tushar Gosavi <tushar@datatorrent.com>
2014-04-18 14:36:26 +0530

    Add copyright and license information.

--- demos/src/main/java/com/datatorrent/demos/uniquevaluetest/RandomDataGenerator.java
+++ demos/src/main/java/com/datatorrent/demos/uniquevaluetest/RandomDataGenerator.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2013 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.datatorrent.demos.uniquevaluetest;
 
 import com.datatorrent.api.Context;
@@ -7,9 +22,7 @@ import com.datatorrent.lib.util.KeyValPair;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by tushar on 7/4/14.
- */
+
 public class RandomDataGenerator implements InputOperator {
     public final transient DefaultOutputPort<KeyValPair<String, Object>> outport = new DefaultOutputPort<KeyValPair<String, Object>>();
     private HashMap<String, Integer> dataInfo;
