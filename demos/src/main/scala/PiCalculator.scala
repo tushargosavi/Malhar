a907592ac163229c0da8d7f2f9ceb08de830e54a
Tushar Gosavi <tushar@datatorrent.com>
2014-08-11 16:15:10 +0530

    Change license and package name for PiCalculator.scala, removed comment from pom.xml

--- demos/src/main/scala/PiCalculator.scala
+++ demos/src/main/scala/PiCalculator.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Tushar R. Gosavi. ALL Rights Reserved.
+ * Copyright (c) 2014 DataTorrent, Inc. ALL Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.tugo.datatorrent.demo.pi
+package com.datatorrent.demos.scala.pi
 
 import com.datatorrent.api.annotation.ApplicationAnnotation
 import com.datatorrent.api.Context.OperatorContext
