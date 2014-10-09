323f73db4ed5ee0b5e7105dc366f4308e6f5c566
Tushar Gosavi <tushar@datatorrent.com>
2014-10-09 15:37:49 +0530

    WAL benchmark application

---
+++ demos/adsdimension/src/main/java/com/datatorrent/demos/adsdimension/waltest/HDSOperator.java
@@ -0,0 +1,42 @@
+package com.datatorrent.demos.adsdimension.waltest;
+
+import com.datatorrent.contrib.hds.AbstractSinglePortHDSWriter;
+import com.datatorrent.contrib.hds.MutableKeyValue;
+import com.datatorrent.lib.codec.KryoSerializableStreamCodec;
+
+import java.util.Arrays;
+
+public class HDSOperator extends AbstractSinglePortHDSWriter<MutableKeyValue>
+{
+  public static class MutableKeyValCodec extends KryoSerializableStreamCodec<MutableKeyValue> implements HDSOperator.HDSCodec<MutableKeyValue>
+  {
+    @Override public byte[] getKeyBytes(MutableKeyValue mutableKeyValue)
+    {
+      return mutableKeyValue.getKey();
+    }
+
+    @Override public byte[] getValueBytes(MutableKeyValue mutableKeyValue)
+    {
+      return mutableKeyValue.getValue();
+    }
+
+    @Override public MutableKeyValue fromKeyValue(byte[] key, byte[] value)
+    {
+      MutableKeyValue pair = new MutableKeyValue(null, null);
+      pair.setKey(key);
+      pair.setValue(value);
+      return pair;
+    }
+
+    @Override public int getPartition(MutableKeyValue tuple)
+    {
+      return Arrays.hashCode(tuple.getKey());
+    }
+  }
+
+
+  @Override protected Class<? extends HDSCodec<MutableKeyValue>> getCodecClass()
+  {
+    return MutableKeyValCodec.class;
+  }
+}
